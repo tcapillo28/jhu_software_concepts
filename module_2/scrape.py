c6d982a7e576923c70308bd492d4fc4f93f55f5b
tonya <tcapill2@jhu.edu>
2026-02-01 10:25:20 -0800

    Only accessing the general admissions board. Not the entries. Must include the page number to iterate through and maintain the API access.

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,20 +1,17 @@
 from urllib.request import Request, urlopen
 import json
 
+#Access to Gradcafe's API
 url = "https://www.thegradcafe.com/api/admissions.php?q=computer"
 
-req = Request(
-    url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
-)
 
+req = Request(url, headers={ "User-Agent": "Mozilla/5.0" })
+
+# Sending the request and decode the JSON response
 response = urlopen(req)
 data = json.loads(response.read().decode("utf-8"))
 
-print(data)
-
+print(json.dumps(data, indent=2))
 
 
 
