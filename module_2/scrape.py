49efd45deaff90d53ad356a64bd5d20865148139
tonya <tcapill2@jhu.edu>
2026-02-01 09:47:08 -0800

    API is identifying our user-agent as a bot since the default user-agent looks like: Python-urlib/3.14
    We will replace it with Mozilla/5.0 as a generic signature so the servers treats our request like a regular human visitor.

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,13 +1,19 @@
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 import json
 
 url = "https://www.thegradcafe.com/api/admissions.php?q=computer"
 
-response = urlopen(url)
+req = Request(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
+)
+
+response = urlopen(req)
 data = json.loads(response.read().decode("utf-8"))
 
 print(data)
-
 
 
 
