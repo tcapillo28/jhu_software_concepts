a0ff715560117675c20c4083aecc53dc7815ea8c
tonya <tcapill2@jhu.edu>
2026-02-08 17:36:07 -0400

    Average calculation error due to ROUND() contains two versions one single argument and another with two arguments. We will use the two arguents to specify decimal places and the first argument is required to be numeric. Must include ROUNT(AVG(gpa)):: numeric,2) instead of ROUND(AVG(gpa), 2) AS avg_gpa,

--- module_3/query_data.py
+++ module_3/query_data.py
@@ -47,7 +47,7 @@ WHERE term = 'Fall 2025';
 
 
 cur.execute(q1)
-print("Question 1. Number of Fall 2025 applicants:", cur.fetchone()[0])
+print("\nQuestion 1. Number of Fall 2025 applicants:", cur.fetchone()[0])
 
 
 # -------------------------------------------------------------------------------------------------------
@@ -73,14 +73,67 @@ rows = cur.fetchall()
 for category, total in rows:
     print(f"{category}: {total}")
 
-"""
-What are the total number of entries from all distinct citizenship categories
-"""
-
 
 """
 Question 2: What percentage of entries are from international students (not American or Other) (to two decimal places)?
 """
+
+q2 = """
+SELECT 
+    ROUND(
+        (SUM(CASE WHEN us_or_international = 'International' THEN 1 ELSE 0 END)::numeric 
+        / COUNT(*) * 100)
+    , 2) AS pct_international
+FROM applicants;
+"""
+cur.execute(q2)
+print("\nQuestions 2. Percentage of entries from international students:",str(cur.fetchone()[0]) + "%")
+
+
+
+# -------------------------------------------------------------------------------------------------------
+#   Questions 3:
+# -------------------------------------------------------------------------------------------------------
+
+'''
+How many entries report their GPA, GRE, GRE V and GRE AW
+'''
+
+print("\n--- Number of applicants who provided each metric ---")
+q3_counts = """
+SELECT
+    COUNT(gpa) AS gpa_count,
+    COUNT(gre) AS gre_count,
+    COUNT(gre_v) AS gre_v_count,
+    COUNT(gre_aw) AS gre_aw_count
+FROM applicants;
+"""
+cur.execute(q3_counts)
+gpa_count, gre_count, gre_v_count, gre_aw_count = cur.fetchone()
+print(f"GPA provided: {gpa_count}")
+print(f"GRE provided: {gre_count}")
+print(f"GRE V provided: {gre_v_count}")
+print(f"GRE AW provided: {gre_aw_count}")
+
+
+"""
+Question 3: What is the average GPA, GRE, GRE V, GRE AW of applicants who provide these metrics?
+"""
+print("\n Question 3. Average GPA, GRE, GRE V, and GRE AW (excluding missing values):")
+q3 = """
+SELECT
+    ROUND(AVG(gpa), 2) AS avg_gpa,
+    ROUND(AVG(gre), 2) AS avg_gre,
+    ROUND(AVG(gre_v), 2) AS avg_gre_v,
+    ROUND(AVG(gre_aw), 2) AS avg_gre_aw
+FROM applicants;
+"""
+cur.execute(q3)
+avg_gpa, avg_gre, avg_gre_v, avg_gre_aw = cur.fetchone()
+print(f"Average GPA: {avg_gpa}")
+print(f"Average GRE Total: {avg_gre}")
+print(f"Average GRE Verbal: {avg_gre_v}")
+print(f"Average GRE AW: {avg_gre_aw}")
 
 
 cur.close()
