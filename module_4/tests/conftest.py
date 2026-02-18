8e3465fad519c886eb5bf3cd0601174ce89e2063
tonya <tcapill2@jhu.edu>
2026-02-18 09:57:00 -0800

    Add percentage to mocked output for formatting test

--- module_4/tests/conftest.py
+++ module_4/tests/conftest.py
@@ -79,4 +79,7 @@ def fake_scrape_output():
 
 @pytest.fixture(autouse=True)
 def mock_full_output(mocker):
-    mocker.patch("src.app.get_full_output", return_value="Mocked output")
+    mocker.patch(
+        "src.app.get_full_output",
+        return_value="Mocked output\nAcceptance rate: 50.00%"
+    )
