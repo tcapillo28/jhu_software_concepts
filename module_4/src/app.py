152dd767724f1c4c5015e843ee2d293b82765d17
tonya <tcapill2@jhu.edu>
2026-02-18 17:19:08 -0800

    need to include buttons to other apps.

--- module_4/src/app.py
+++ module_4/src/app.py
@@ -99,4 +99,4 @@ def register_routes(app):
 # ---------------------------------------------------------
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
